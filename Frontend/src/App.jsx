5d14e6e3c12f869caef611bce7b5825baa9fb9a5
shreeshailmiragi18 <shreeshailbm18@gmail.com>
2025-10-11 12:37:08 +0530

    modified app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import './App.css'
 import Navigation from './Customer/Components/Navigation/Navigation'
+import HomePage from './Customer/Pages/HomePage/HomePage'
 
 function App() {
 
 
   return (
-    <>
+    <div className="div">
       <Navigation />
-    </>
-     
+      <div>
+        <HomePage />
+      </div>
+    </div>
     
   )
 }
